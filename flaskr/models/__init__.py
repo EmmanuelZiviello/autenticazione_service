3a3f808b8bfbdba16af56f5b02eabee511be1399
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-02-12 14:08:40 +0100

    Add files via upload

---
+++ flaskr/models/__init__.py
@@ -0,0 +1,4 @@
+from flaskr.models.consensi_utente import ConsensiUtenteModel
+from flaskr.models.nutrizionista import NutrizionistaModel
+from flaskr.models.paziente import PazienteModel
+from flaskr.models.richiesta_aggiunta_paziente import RichiestaAggiuntaPazienteModel
