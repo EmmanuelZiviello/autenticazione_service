37db431a600a35e0a9fa2958b764d37e71c26fc3
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-02-13 20:28:34 +0100

    Update __init__.py

--- flaskr/models/__init__.py
+++ flaskr/models/__init__.py
@@ -1,4 +1,3 @@
-from flaskr.models.consensi_utente import ConsensiUtenteModel
 from flaskr.models.nutrizionista import NutrizionistaModel
 from flaskr.models.paziente import PazienteModel
 from flaskr.models.richiesta_aggiunta_paziente import RichiestaAggiuntaPazienteModel
