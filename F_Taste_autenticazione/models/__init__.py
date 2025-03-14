268a2cc26a97238cbafdd2d46a8a77617e803bda
EmmanuelZiviello <124155626+EmmanuelZiviello@users.noreply.github.com>
2025-03-14 17:14:10 +0100

    Delete F_Taste_autenticazione directory

--- F_Taste_autenticazione/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from F_Taste_autenticazione.models.nutrizionista import NutrizionistaModel
-from F_Taste_autenticazione.models.paziente import PazienteModel
--- F_Taste_autenticazione/repositories/admin_repository.py
+++
@@ -1,6 +0,0 @@
-from F_Taste_autenticazione.utils.credentials import admin_id, admin_password
-
-class AdminRepository:
-    @staticmethod
-    def get_admin_credentials():
-        return admin_id, admin_password
